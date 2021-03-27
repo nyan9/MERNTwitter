cdb69a9163b6d728a6a7e7a3a3e678e398bc6145
nyan9 <rynnaing@gmail.com>
2021-03-27 17:22:52 -0400

    add jwt auth

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
@@ -9,6 +14,17 @@ mongoose
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("adsfasdfsadfasdf"));
+// app.get("/", (req, res) => {
+//   res.send("adsfasdfsadfasdf");
+// });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
